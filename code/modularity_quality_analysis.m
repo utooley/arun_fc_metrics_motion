b7ac6dd4b077435bacb94b1539ed0d9e7769b906
Ursula Tooley <utooley@gmail.com>
2019-05-05 09:58:52 -0400

    initialized repo

---
+++ code/modularity_quality_analysis.m
@@ -0,0 +1,24 @@
+%% SETUP
+subj_dir=
+data_dir=
+
+
+%subject list
+S1200_Release_Subjects_Demographics.csv
+%%
+%for each subject
+
+%for each FC metric
+
+%calculate the modularity quality index raw on each metric
+%probably won't use this, but worth having
+
+    %cycle through to ensure that the mean number of communities is 7
+    %(mean across iterations of Gen Louvain, or mean across subjects)?
+
+%rank order the weights in the Pearson correlation matrix
+
+%replace the rank ordered weights in the xxx correlation matrix with the
+%Pearson correlation matrix
+
+%calculate modularity on the re-weighted xxx matrix
